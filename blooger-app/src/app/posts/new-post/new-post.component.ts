617a3c44f5fdf5f27d0c16aad7a05ff17c9a18f3
khaled <khaledleader012@gmail.com>
2025-05-13 16:45:32 +0300

    implement add posts method

--- blooger-app/src/app/posts/new-post/new-post.component.ts
+++ blooger-app/src/app/posts/new-post/new-post.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-new-post',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.css'
 })
+  
 export class NewPostComponent {
+  @Output() add = new EventEmitter<{title:string, content:string , date:string}>();
     
     titleInput: string = '';
     contentInput: string = '';
-  isAdded: boolean = true;
+    isAdded: boolean = true;
   
-  addPost () {
-    this.titleInput = '';
-    this.contentInput = '';
-    this.isAdded = false;
+  
+  addPost() {
+    this.add.emit({
+      title: this.titleInput,
+      content: this.contentInput,
+      date: new Date().toISOString()
+    });
+    console.log('Post added:',  this.titleInput)
   }
 
 }
